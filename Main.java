d3f397ccb36d18bd8dd041b86dff60e2b5452571
Stefan Wagner <112750321+Stdehnbostel@users.noreply.github.com>
2023-11-03 10:06:23 +0100

    Zusammengebastelter Server

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    
+
+    public static void main(String args[]) {
+
+        ArrayList<ServerThread> clients = new ArrayList<ServerThread>();
+        String input = "";
+        try (ServerSocket server = new ServerSocket(1234)){
+            
+            while(!input.equals("exit")) {
+                Socket client = server.accept();
+                Thread newUser = new Thread(() -> receiveUserAndPwd(client, clients));
+                newUser.start();
+                
+            }
+        } catch (Exception e) {
+            System.out.println("Error occured in main: " + e.getStackTrace());
+        }
+    }
+
+    private static void receiveUserAndPwd(Socket client, ArrayList<ServerThread> clients) {
+
+        try {
+
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            DataInputStream in = new DataInputStream(client.getInputStream());
+
+            System.out.println("Ask for username");
+            out.writeUTF("Nutzernamen eingeben: ");
+            String userName = in.readUTF();
+
+            out.writeUTF("Passwort eingeben: ");                
+            String pwd = in.readUTF();
+
+            System.out.println("Name: " + userName + " pwd: " + pwd);
+
+            ServerThread comThread = new ServerThread(client, clients, userName, pwd);
+            clients.add(comThread);
+            comThread.start();
+
+        } catch (IOException e) {
+
+        }
+    }
+}
---
+++ ServerThread.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerThread extends Thread {
+
+    private Socket client;
+    private ArrayList<ServerThread> threadList;
+    private PrintWriter output;
+    public String userName;
+    private String pwd;
+
+    public ServerThread(Socket socket, ArrayList<ServerThread> threads, String name, String pwd) {
+        this.client = socket;
+        this.threadList = threads;
+        this.userName = name;
+        this.pwd = pwd;
+    }
+
+    @Override
+    public void run() {
+        try {
+            //Reading the input from Client
+            BufferedReader input = new BufferedReader( new InputStreamReader(client.getInputStream()));
+            
+            //returning the output to the client : true statement is to flush the buffer otherwise
+            //we have to do it manually
+             output = new PrintWriter(client.getOutputStream(),true);
+
+
+            //inifite loop for server
+            while(true) {
+                String outputString = input.readLine();
+                //if user types exit command
+                if(outputString.equals("exit")) {
+                    break;
+                }
+                printToALlClients(outputString);
+                //output.println("Server says " + outputString);
+                System.out.println("Server received " + outputString);
+
+            }
+
+
+        } catch (Exception e) {
+            System.out.println("Error occured " +e.getStackTrace());
+        }
+    }
+
+    private void printToALlClients(String outputString) {
+        for( ServerThread sT: threadList) {
+            sT.output.println(outputString);
+        }
+
+    }
+}
