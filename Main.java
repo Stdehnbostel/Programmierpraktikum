f137b91d328e8c0823e8dec3676cfcc88f01221e
AngeloSunn <64798061+AngeloSunn@users.noreply.github.com>
2023-11-09 02:24:32 +0100

    new login/regi logic, better looking welcome msgs
    needs bug fixed:
    login bug (User won't be able to send / receive messages after logging back into an already existing account, likely due to mismatched DataOutput/DataInput streams)
     possible fix: store DataOutput / Input streams in ServerThread class, then overwrite the out and in variables with the already existing ones - if the username already exists of course.

--- Main.java
+++ Main.java
@@ -22,46 +22,70 @@ public class Main {
     }
 
     private static void receiveUserAndPwd(Socket client, ArrayList<ServerThread> clients) {
-
         try {
-
             DataOutputStream out = new DataOutputStream(client.getOutputStream());
             DataInputStream in = new DataInputStream(client.getInputStream());
-
-            //abfrage-Kommunikation durch ClientMain
-            boolean nameAvailable = true;
-            System.out.println("Ask for username");
+    
+            // Prompt the client for the username
             out.writeUTF("Nutzernamen eingeben:");
             String userName = in.readUTF();
-
-            out.writeUTF("Passwort eingeben:");                
-            String pwd = in.readUTF();
-
-            System.out.println("Name: " + userName + " pwd: " + pwd);
-
-            StringBuilder users = new StringBuilder();
-            users.append("Auf dem Server:\n");
-            ServerThread comThread = new ServerThread(client, clients, userName, pwd);
+    
+            boolean nameAvailable = true;
+         
+            ServerThread comThread = null;
             
-            for(ServerThread sT: clients) {
-                if(sT.userName.equals(userName) && !sT.getPwd().equals(pwd)) {
+            // Check if the username is already in use
+            for (ServerThread sT : clients) {
+                if (sT.userName.equals(userName)) {
                     nameAvailable = false;
+                    comThread = sT;
+                    // Ich glaube hier müssten die Streams überschrieben werden mit den alten
+                    // out = sT.out;
+                    // in = sT.in;
+                    // (so ungefähr?)
+                    break;
                 }
-                users.append(sT.userName + "\n");
             }
-            if(nameAvailable == false) {
-                out.writeUTF("Der Name existiert bereits.");
-                receiveUserAndPwd(client, clients);
-            } else {
-                 sendServerMessage(clients, userName + " hat sich neu angemeldet");
-                out.writeUTF(users.toString());
+    
+            if (nameAvailable) {
+                // If the username is not in use, prompt the client to register 
+                out.writeUTF("Registriere dich mit deinem Passwort:");
+                String pwd = in.readUTF();
+                comThread = new ServerThread(client, clients, userName, pwd);
                 clients.add(comThread);
                 comThread.start();
+                sendServerMessage(clients, "* " + userName + " hat sich registriert! *");
+            } else {
+                // If the username is already in use, prompt the client for authentication
+                out.writeUTF("Log dich mit deinem Passwort ein:");
+                String pwd = in.readUTF();
+                if (comThread.getPwd().equals(pwd)) {
+                    // Password is correct
+                    out.writeUTF("Login erfolgreich!");
+                    sendServerMessage(clients, "* " + userName + " hat sich angemeldet! *");
+                } else {
+                    // Password is incorrect
+                    out.writeUTF("Falsches Passwort! Verbindung wird beendet.");
+                    // clients.remove(comThread) | dachte zuerst das würde Sinn ergeben, löscht aber natürlich den ganzen User, also müsste sich der Client neu registrieren;
+                    client.close();
+                }
+                
             }
+    
+            // Provide the list of users
+            // todo: Online-Status in Client mit einbauen, und nur auf die userlist schreiben, falls der client wirklich online ist.
+            // zur Zeit werden registrierte user, welche gar nicht online sind auch mit aufgeschrieben
+            StringBuilder users = new StringBuilder("Auf dem Server:\n");
+            int userNumber = 1;
+            for (ServerThread sT : clients) {
+                users.append(userNumber).append(". [").append(sT.userName).append("]\n");
+                userNumber++;
+            }
+        out.writeUTF(users.toString());
         } catch (IOException e) {
-
         }
     }
+    
 
     private static void sendServerMessage(ArrayList<ServerThread> clients, String msg) {
         try {
